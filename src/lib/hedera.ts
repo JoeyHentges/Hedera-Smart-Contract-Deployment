c2f81c3a61693266c969bb228669ea7ff3c1a6be
Joe Hentges <30447791+JoeyHentges@users.noreply.github.com>
2021-11-27 02:01:21 -0600

    init

---
+++ src/lib/hedera.ts
@@ -0,0 +1,24 @@
+import { Client } from '@hashgraph/sdk';
+import { config } from 'dotenv';
+
+const environment = process.env.NODE_ENV;
+config({ path: environment === 'development' ? './.env.development' : './.env.production' });
+
+const accountId: string = process.env.ACCOUNT_ID || '';
+const privateKey: string = process.env.PRIVATE_KEY || '';
+
+let client;
+
+try {
+  if (environment === 'development') {
+    client = Client.forTestnet().setOperator(accountId, privateKey);
+  } else {
+    client = Client.forMainnet().setOperator(accountId, privateKey);
+  }
+} catch {
+  throw new Error(
+    "Environment variables HEDERA_NETWORK, OPERATOR_ID, and OPERATOR_KEY are required."
+  );
+}
+
+export { client };
